a92ff07b2e5d6f708b2f5ab5d4d53e3cc1e08fed
samitmohan <samitmohan@gmail.com>
2025-04-29 14:19:47 +0530

    daily lc

---
+++ Problems/Sliding/countSubarraysLeastK.py
@@ -0,0 +1,20 @@
+# 2962. Count Subarrays Where Max Element Appears at Least K Times
+
+# https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        max_num = max(nums)
+        # now its just sliding window
+        left = 0
+        ans = 0
+        count = 0 
+        for right in range(len(nums)):
+            if nums[right] == max_num:
+                count += 1
+            while count >= k:
+                ans += len(nums) - right
+                if nums[left] == max_num: 
+                    count -= 1
+                left += 1
+        return ans
+ts 
---
+++ PythonLanguageSpecific/prettyRecursion.py
@@ -0,0 +1,22 @@
+from recviz import recviz
+
+@recviz
+def fib(n):
+  # base condition mimicking the first two numbers
+  # in the sequence
+  if n == 0: return 0
+  if n == 1: return 1
+
+  # every number is summation of the previous two
+  return fib(n - 1) + fib(n - 2)
+
+#  -> fib(3)
+#     -> fib(2)
+#        -> fib(1)
+#        <- 1
+#        -> fib(0)
+#        <- 1
+#     <- 2
+#     -> fib(1)
+#     <- 1
+#  <- 3
