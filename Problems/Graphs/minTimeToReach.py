733e644e18cba6780649a48c984531a8b3147a58
samitmohan <samitmohan@gmail.com>
2025-05-08 15:48:22 +0530

    hellooo

--- Problems/Graphs/minTimeToReach.py
+++ Problems/Graphs/minTimeToReach.py
@@ -1,7 +1,122 @@
+# Both Problems
 # https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/description/?envType=daily-question&envId=2025-05-07
+# https://leetcode.com/problems/find-minimum-time-to-reach-last-room-ii/?envType=daily-question&envId=2025-05-08
+
+#  and two seconds for the next, alternating between the two -> keep parity
+from collections import defaultdict
+import heapq
+# bfs + min_heap
+'''
+moveTime = [[0,4],[4,4]]
+Output 5 Expected 6
+'''
+from heapq import heappush, heappop
+def minTimeToReach(moveTime):
+    num_rows = len(moveTime)
+    num_cols = len(moveTime[0])
+    dirns = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    inf = float('inf')
+    cache = [[inf] * num_cols for _ in range(num_rows)] # answer array
+    cache[0][0] = 0
+    heap = []
+    heappush(heap, (0, 0, 0)) # current_time, row, col
+    while heap:
+        currTime, row, col = heappop(heap) # get the current cost row col
+        # now need to check boundaries
+        if row == num_rows - 1 and col == num_cols - 1: return currTime
+        if currTime > cache[row][col]: continue
+
+        for dr, dc in dirns:
+            new_row = row + dr
+            new_col = col + dc
+            if 0 <= new_row < num_rows and 0 <= new_col < num_cols:
+                next_time = max(currTime, moveTime[new_row][new_col]) + 1
+                if next_time < cache[new_row][new_col]:
+                    cache[new_row][new_col] = next_time
+                    heappush(heap, (next_time, new_row, new_col))
+    return -1
+        
+
+'''
+Now for next problem -> Can move 1 step or 2 step dependening on parity.
+move 1 = 1 
+move 2 = 2
+move 3 = 1
+# need to add parity bit in heap
+move_duration = 1 if parity % 2 != 0 else 2
+'''
+
+def minTimeToReach2(moveTime):
+    nr, nc = len(moveTime), len(moveTime[0])
+    dirns = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    inf = float('inf')
+    cache = [[inf] * nc for _ in range(nr)] # answer array
+    cache[0][0] = 0
+    mh = []
+    heappush(mh, (0, 0, 0, 0)) # currtime, row, col, parity
+    while mh:
+        currTime, row, col, parity = heappop(mh)
+        if row == nr - 1 and col == nc - 1: return currTime
+        if currTime > cache[row][col]: continue
+        for dr, dc in dirns:
+            new_row = row + dr
+            new_col = col + dc
+            if 0 <= new_row < nr and 0 <= new_col < nc: # in boundary
+                moveDuration = 1 if parity == 0 else 2
+                nextTime = max(currTime, moveTime[new_row][new_col]) + moveDuration
+                if nextTime < cache[new_row][new_col]:
+                    cache[new_row][new_col] = nextTime # djkstra
+                    new_parity = 1 - parity 
+                    heappush(mh, (nextTime, new_row, new_col, new_parity))
+    return -1
+
+
+
+def main():
+    print(minTimeToReach(moveTime=[[0,4],[4,4]])) # 6
+    print(minTimeToReach2( moveTime = [[0,4],[4,4]])) # 7
+main()
+
+            
+        
 
 
 '''
+notes
+
+You're in the top-left room of a grid, and you want to reach the bottom-right room as fast as possible.
+But here's the twist:
+    Each room has a timer (moveTime[r][c]) that tells you the earliest time you can enter it.
+    AND every move you make alternates between taking 1 second, then 2 seconds, then 1, then 2...
+        First move: 1 sec
+        Second move: 2 sec
+        Third move: 1 sec
+        Fourth move: 2 sec
+
+
+while there are still rooms in the heap:
+    get the room with the smallest time
+
+    if we already visited this room with this parity, skip
+
+    if we're at the goal (bottom-right), return the time
+
+    for each direction (up/down/left/right):
+        check if the next room is inside the grid
+        figure out:
+            - how long you must wait before it's unlocked (from moveTime)
+            - how long this move takes (1 sec if even step, 2 if odd)
+            - total time = max(current time, unlock time) + move duration
+            - next parity flips (even ↔ odd)
+
+        if we haven't visited next room with that parity:
+            push it into the heap
+Each room can be visited twice:
+    Once when it’s your turn to make a 1-sec move.
+    Once when it’s a 2-sec move.
+
+And those two paths may lead to totally different futures.
+
 takes one second to move
 if curr_time >= next destination time -> simply add 1 to timer and jump to next destination
 if not then add next_destination_time to current timer = t += next_dest_time + 1
@@ -15,60 +130,5 @@ visited array= false
 minheap = (0,0,0) {row, col, time}
 curr time = max(curr_time, nextlocation) + 1
 TC : O((nm)lognm) nm due to min heap
+
 '''
-import heapq
-class Solution:
-    def minTimeToReach(self, moveTime):
-        m, n = len(moveTime), len(moveTime[0]) # row, col
-        dir = [-1, 0, 1, 0, -1]
-        def isValid(row, col, m, n):
-            return 0 <= row < m and 0 <= col < n
-        minHeap = [(0,0,0)] # (time, row, col)
-        vis = [[False] * n for _ in range(m)]
-        vis[0][0] = True
-        while minHeap:
-            time, r, c = heapq.heappop(minHeap)
-            if r == m-1 and c == n-1: return time # ans
-            for i in range(4):
-                new_row, new_col = r + dir[i], c + dir[i+1]
-                if isValid(new_row, new_col, m, n) and not vis[new_row][new_col]:
-                    new_time = 1 + max(time, moveTime[new_row][new_col])
-                    heapq.heappush(minHeap, (new_time, new_row, new_col))
-                    vis[new_row][new_col] = True
-        return -1
-
-
-# more on dj
-
-class Solution:
-    def minTimeToReach(self, moveTime):
-        inf = float('inf')
-        num_rows, num_cols  = len(moveTime), len(moveTime[0])
-        cache = [[inf] * num_cols for _ in range(num_rows)]
-        min_heap = []
-        heapq.heappush(min_heap, (0, 0, 0)) # timecost, newrow, newcol
-        while min_heap:
-            cost, row, col = heapq.heappop(min_heap)
-            if row == num_rows-1 and col == num_cols-1: # ans found
-                continue
-            
-            for _row, _col in [[-1, 0], [1, 0], [0, 1], [0, -1]]: # new row, new col
-                new_row = _row + row
-                new_col = _col + col
-                if new_row < 0 or new_col < 0 or new_row == num_rows or new_col == num_cols: # boundary check
-                    continue
-                
-                new_cost = max(moveTime[new_row][new_col], cost) + 1
-                if new_cost < cache[new_row][new_col]:
-                    cache[new_row][new_col] = new_cost # update
-                    heapq.heappush(min_heap, (new_cost, new_row, new_col))
-        return cache[-1][-1]
-
-
-
-s = Solution()
-print(s.minTimeToReach(moveTime = [[0,4],[4,4]]))
-
-
-
-        
